11af7c19c01ecc04bdac0aa97f90c61f37129937
naija963@gmail.com <naija963@gmail.com>
2023-09-16 21:10:04 +0100

    projet done

--- src/App.js
+++ src/App.js
@@ -1,54 +1,99 @@
-
 import './App.css';
 import {useState} from "react";
+import axios from 'axios';
+
 
 function App() {
-    const [environmentName, setEnvironmentName] = useState('');
-    const [selectedScenario, setSelectedScenario] = useState('');
-    const [selectedContainerImage, setSelectedContainerImage] = useState('');
+    const [environment, setEnvironment] = useState({
+        environmentName: '',
+        selectedScenario: '',
+        selectedContainerImage: ''
 
-    const handleSubmit = () => {
+    });
+
+    const handlechange = (e) => {
+        const { name, value } = e.target
+        setEnvironment({...environment,
+            [name]: value
+        })
+    }
+
+
+    let images = ["image-1", "image-2", "image-3"];
+    let types = ["Kubernetes RBAC", "Dockerfile"]
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        try {
+            const response = await axios.post('https://my-backend.com/api/endpoint', {
+                "environment": environment,
+            }, {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+
+            
+            console.log('Réponse du serveur :', response.data);
+        } catch (error) {
+            
+            console.error('Erreur lors de la requête POST :', error);
+            
+        }
 
 
     };
     return (
         <form onSubmit={handleSubmit}>
-            <div>
-                <label htmlFor="environmentName">Environment Name:</label>
-                <input
-                    type="text"
-                    id="environmentName"
-                    value={environmentName}
-                    onChange={(e) => setEnvironmentName(e.target.value)}
-                />
-            </div>
-            <div>
-                <label htmlFor="scenario">Scenario:</label>
-                <select
-                    id="scenario"
-                    value={selectedScenario}
-                    onChange={(e) => setSelectedScenario(e.target.value)}
-                >
-                    <option value="k8s_rbac">Kubernetes RBAC</option>
-                    <option value="dockerfile">Dockerfile</option>
-                    {/* Add more scenario options here */}
-                </select>
-            </div>
-            <div>
-                <label htmlFor="containerImage">Container Image:</label>
-                <select
-                    id="containerImage"
-                    value={selectedContainerImage}
-                    onChange={(e) => setSelectedContainerImage(e.target.value)}
-                >
-                    <option value="image-1">Image 1</option>
-                    <option value="image-2">Image 2</option>
-                    {/* Add more container image options here */}
-                </select>
-            </div>
-            <button type="submit">Submit</button>
-        </form>
-    );
+
+                <div className="sm:px-12 px-6  my-12 sm:border border-gray-900/10 sm:py-24 max-w-xl mx-auto grid grid-cols-1 gap-y-8 sm:gap-y-12 sm:grid-cols-6 sm:shadow-[0_8px_30px_rgb(0,0,0,0.12)] rounded-xl">
+                   <h1 className="sm:col-span-6 text-center text-2xl sm:text-4xl font-bold text-gray-900 pb-2 ">Environment configuration</h1>
+                    <div className="sm:col-span-6">
+                        <label className="block text-base font-medium leading-10 text-gray-900" htmlFor="environmentName">Environment Name:</label>
+                        <input className="block px-4 w-full rounded-md border py-1.5 text-gray-900 shadow-sm ring-inset focus:border-0 border-gray-200 placeholder:text-gray-400 focus:ring-2 focus:outline-none focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                               type="text"
+                               required
+                               placeholder="Enter environment name"
+                               id="environmentName"
+                               value={environment.environmentName}
+                               onChange={handlechange}
+                        />
+                    </div>
+                    <div className="sm:col-span-6 flex sm:flex-row flex-col sm:gap-12 gap-6  ">
+                        <label className="text-base font-medium sm:py-2 " htmlFor="scenario">Scenario:</label>
+                        <select
+                            id="scenario"
+                            value={environment.selectedScenario}
+                            className="  flex-auto cursor-pointer shadow-sm py-3 px-4 pr-9 block w-full border-gray-200 rounded-md border focus:border-0 text-sm focus:border-gray-800 focus:ring-indigo-500  focus:outline-none focus:ring-2 focus:ring-offset-2 bg-white"
+                            onChange={handlechange}
+                        >
+                            {
+                                types.map((image) => {return(<option value={image}>{image}</option>)})
+                            }
+
+                        </select>
+                    </div >
+                    <div className="sm:col-span-6 flex sm:flex-row flex-col sm:gap-12 gap-6 ">
+                        <label className="text-base font-medium " htmlFor="containerImage">Container Image:</label>
+                        <select
+                            id="containerImage"
+                            value={environment.selectedContainerImage}
+                            className="flex-auto cursor-pointer shadow-sm py-3 px-4 pr-9 block w-full border-gray-200 rounded-md border focus:border-0 text-sm focus:border-gray-800 focus:ring-indigo-500  focus:outline-none focus:ring-2 focus:ring-offset-2 bg-white"
+                            onChange={handlechange}
+                        >
+                            {
+                                images.map((image) => {return(<option value={image}>{image}</option>)})
+                            }
+
+                        </select>
+                    </div>
+                    <div className="sm:col-span-6 flex items-center justify-center ">
+                        <button className=" rounded-md bg-indigo-600 px-14 py-3 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" type="submit">Submit</button>
+                    </div>
+                </div>
+
+        </form>);
 }
 
 export default App;
